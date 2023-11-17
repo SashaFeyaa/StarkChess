04040ddf568af043b3a2851a39c874d9b31128a8
SashaFeyaa <sashulya02@gmail.com>
2023-11-17 18:28:37 +0200

    PvP backend

--- SCfrontend/starkchess/starkchess/src/components/PvP.js
+++ SCfrontend/starkchess/starkchess/src/components/PvP.js
@@ -1,116 +1,81 @@
 import { useState, useMemo, useCallback, useEffect } from "react";
-import { Container } from "@mui/material";
+import { Container, TextField } from "@mui/material";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
+import Game from "./Game.js";
+import socket from '../socket.js';
+import InitGame from "./InitGame.js";
 import CustomDialog from "./CustomDialog";
 import Grid from '@mui/material/Grid'; // Import Material-UI Grid
 
-function PvP({ players, room, orientation, cleanup }) {
-  const chess = useMemo(() => new Chess(), []); // <- 1
-  const [fen, setFen] = useState(chess.fen()); // <- 2
-  const [over, setOver] = useState("");
+function PvP() {
+  const [username, setUsername] = useState("");
+  const [usernameSubmitted, setUsernameSubmitted] = useState(false);
 
-  const pieces = [
-    "wP",
-    "wN",
-    "wB",
-    "wR",
-    "wQ",
-    "wK",
-    "bP",
-    "bN",
-    "bB",
-    "bR",
-    "bQ",
-    "bK",
-  ];
-  
-  const customPieces = useMemo(() => {
-    const pieceComponents = {};
-    pieces.forEach((piece) => {
-      pieceComponents[piece] = ({ squareWidth }) => (
-        <div
-          style={{
-            width: squareWidth,
-            height: squareWidth,
-            backgroundImage: `url(/imgbg/pieces/${piece}.png)`,
-            backgroundSize: "100%",
-          }}
-        />
-      );
-    });
-    return pieceComponents;
+  const [room, setRoom] = useState("");
+  const [orientation, setOrientation] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  // resets the states responsible for initializing a game
+  const cleanup = useCallback(() => {
+    setRoom("");
+    setOrientation("");
+    setPlayers("");
   }, []);
 
-  function onDrop(sourceSquare, targetSquare) {
-    const moveData = {
-      from: sourceSquare,
-      to: targetSquare,
-      color: chess.turn(),
-      // promotion: "q",
-    };
+  useEffect(() => {
+    // const username = prompt("Username");
+    // setUsername(username);
+    // socket.emit("username", username);
 
-    const move = makeAMove(moveData);
+    socket.on("opponentJoined", (roomData) => {
+      console.log("roomData", roomData)
+      setPlayers(roomData.players);
+    });
+  }, []);
 
-    // illegal move
-    if (move === null) return false;
-
-    return true;
-  }
-
-  const makeAMove = useCallback(
-    (move) => {
-      try {
-        const result = chess.move(move); // update Chess instance
-        setFen(chess.fen()); // update fen state to trigger a re-render
-  
-        console.log("over, checkmate", chess.isGameOver(), chess.isCheckmate());
-  
-        if (chess.isGameOver()) { // check if move led to "game over"
-          if (chess.isCheckmate()) { // if reason for game over is a checkmate
-            // Set message to checkmate. 
-            setOver(
-              `Checkmate! ${chess.turn() === "w" ? "black" : "white"} wins!`
-            ); 
-            // The winner is determined by checking which side made the last move
-          } else if (chess.isDraw()) { // if it is a draw
-            setOver("Draw"); // set message to "Draw"
-          } else {
-            setOver("Game over");
-          }
-        }
-  
-        return result;
-      } catch (e) {
-        return null;
-      } // null if the move was illegal, the move object if the move was legal
-    },
-    [chess]
-  );
-    
   // Game component returned jsx
   return (
     <Container>
-      <Grid  className='chessboard'  >
-        <Chessboard position={fen} onPieceDrop={onDrop} id="PlayVsStockfish"
-            className="chessboard"
-            customBoardStyle={{
-              width: "100%",
-              borderRadius: "4px",
-              boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
-            }}
-            customDarkSquareStyle={{ backgroundColor: "#CECEDA" }}
-            customLightSquareStyle={{ backgroundColor: "#FAFAFA" }}
-            customPieces={customPieces}/>  {/**  <- 4 */}
-        </Grid>
+      {room ? (
+        <Game
+        room={room}
+        orientation={orientation}
+        username={username}
+        players={players}
+        // the cleanup function will be used by Game to reset the state when a game is over
+        cleanup={cleanup}
+      /> ) : (
+        <InitGame
+          setRoom={setRoom}
+          setOrientation={setOrientation}
+          setPlayers={setPlayers}
+        />
+      )}
       <CustomDialog // <- 5
-        open={Boolean(over)}
-        title={over}
-        contentText={over}
-        handleContinue={() => {
-          setOver("");
-        }}
-      />
+         open={!usernameSubmitted} // leave open if username has not been selected
+         title="Pick a username" // Title of dialog
+         contentText="Please select a username" // content text of dialog
+         handleContinue={() => { // fired when continue is clicked
+           if (!username) return; // if username hasn't been entered, do nothing
+           socket.emit("username", username); // emit a websocket event called "username" with the username as data
+           setUsernameSubmitted(true); // indicate that username has been submitted
+         }}
+         >
+         <TextField // Input
+           autoFocus // automatically set focus on input (make it active).
+           margin="dense"
+           id="username"
+           label="Username"
+           name="username"
+           value={username}
+           required
+           onChange={(e) => setUsername(e.target.value)} // update username state with value
+           type="text"
+           fullWidth
+           variant="standard"
+         />
+       </CustomDialog>
     </Container>
   );
 }
